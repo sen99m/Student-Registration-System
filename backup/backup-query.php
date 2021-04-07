c47e80a87c1571ce687976bd42b94d3a57175a6a
sen99m <senmadhurima99@gmail.com>
2021-04-07 22:30:17 +0530

    First commit

--- backup/backup-query.php
+++
@@ -1,5 +0,0 @@
-SELECT student_info_master.sid, student_info_master.fname, student_info_master.mname, student_info_master.lname, student_info_master.dob, student_info_master.gender, student_info_master.mobilenumber, student_info_master.email, student_info_master.address, student_info_master.subject, district_master.dname
-FROM student_info_master
-LEFT JOIN district_master
-ON district_master.did = student_info_master.district;
-
